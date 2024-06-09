5e7934fa9a664e409a60c8f48ef82b555a5e1252
omarelladen <109101498+omarelladen@users.noreply.github.com>
2024-06-09 12:43:35 -0300

    3 - td sincronizado a partir de 50 MHz (eu acho)
    falta o detector

--- freq8.vhd
+++ freq8.vhd
@@ -2,13 +2,13 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
   
-entity freq1 is
+entity freq8 is
 	port ( clk: in std_logic; -- Pin connected to P11 (N14)
 			clk_10kHz: out std_logic);-- Can check it using PIN A8 - LEDR0
 
-	end freq1;
+	end freq8;
   
-architecture freq_div of freq1 is
+architecture freq_div of freq8 is
   
 signal count: integer:=1;
 signal tmp : std_logic := '0';
@@ -19,7 +19,7 @@ process(clk)
 	begin
 	if(clk'event and clk='1') then
 		count <=count+1;
-		if (count = 50000000/(2*3125000)) then -- (3.125 MHz)
+		if (count = 50000000/(2*25000000)) then -- (25 MHz)
 			tmp <= NOT tmp;
 			count <= 1;
 		end if;
